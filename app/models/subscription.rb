bad38a961e8b2209e8a3718c7e391b7ff8586a8b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-11 20:21:13 -0600

    define status as enum and test

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,5 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+  enum status: { inactive: 0, active: 1 }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   it {should have_many(:teas).through(:subscription_teas)}
   it {should belong_to :customer }
-  it {should define_enum_for(:status).with([:active, :inactive])}
+  it {should define_enum_for(:status).with_values(inactive: 0, active: 1) }
 end
