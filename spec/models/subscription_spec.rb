73e007346a758b4d808e907e18de255c3656693c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-11 20:39:03 -0600

    refactor subscription_teas factory, test subscription cost

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Subscription, type: :model do
   it {should have_many(:teas).through(:subscription_teas)}
   it {should belong_to :customer }
   it {should define_enum_for(:status).with_values(inactive: 0, active: 1) }
+
+  describe 'methods' do 
+    before(:each) do 
+      @tea_1 = create(:tea, price: 10.00)
+      @tea_2 = create(:tea, price: 12.00)
+      @tea_3 = create(:tea, price: 9.50)
+
+      @customer = create(:customer)
+      @subscription = create(:subscription, customer: @customer)
+      @st_1 = create(:subscription_tea, subscription: @subscription, tea: @tea_1)
+      @st_2 = create(:subscription_tea, subscription: @subscription, tea: @tea_2)
+      @st_3 = create(:subscription_tea, subscription: @subscription, tea: @tea_3)
+    end
+
+    it 'tallies cost based on tea prices' do 
+      expect(@subscription.cost).to eq(31.50)
+    end
+  end
 end
