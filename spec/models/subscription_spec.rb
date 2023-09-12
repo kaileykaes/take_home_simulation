2cbba84d06becaad4a78bc0e018068a10098363f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-11 20:10:59 -0600

    test: subscription status is enum

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   it {should have_many(:teas).through(:subscription_teas)}
   it {should belong_to :customer }
+  it {should define_enum_for(:status).with([:active, :inactive])}
 end
