bec670711cd7ff022bf2591347757c28e5cae754
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-11 20:48:06 -0600

    create cost method for subscription model

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,6 @@ frequencies = ['daily', 'weekly', 'fortnightly', 'monthly']
 
 FactoryBot.define do
   factory :subscription do
-    cost { Faker::Number.decimal(l_digits: 2, r_digits: 1) }
     frequency { frequencies.sample }
     status { [0, 1].sample }
     title { Faker::Music.album }
