676175a82f5858a3e78fa6ec17f8843e1219395d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-14 22:07:51 -0600

    rescue from record not found and routing error with 404 response, refactor json rendering into private method for dry-er code

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::BaseController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record_response
+  rescue_from ActionController::RoutingError, with: :no_record_response
 
   def invalid_record_response(error)
-    render json: ErrorSerializer.serialize_error(error, 422)
+    base_response(error, 422)
     response.status = 422
+  end
+
+  def no_record_response(error)
+    base_response(error, 404)
+    response.status = 404
+  end
+
+  private
+  def base_response(error, status)
+    render json: ErrorSerializer.serialize_error(error, status)
   end
 end
