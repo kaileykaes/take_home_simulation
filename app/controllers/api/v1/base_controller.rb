83f89940a7282af1fef167803d84d91593abc154
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-15 12:48:58 -0600

    rename private method from base_response to render_error

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,17 +4,17 @@ class Api::V1::BaseController < ApplicationController
   rescue_from ActionController::RoutingError, with: :no_record_response
 
   def invalid_record_response(error)
-    base_response(error, 422)
+    render_error(error, 422)
     response.status = 422
   end
 
   def no_record_response(error)
-    base_response(error, 404)
+    render_error(error, 404)
     response.status = 404
   end
 
   private
-  def base_response(error, status)
+  def render_error(error, status)
     render json: ErrorSerializer.serialize_error(error, status)
   end
 end
